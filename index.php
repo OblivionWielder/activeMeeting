0efb199fa853aae283502c4c561fbd68550d5b4e
Ana <ana_egm13@hotmail.com>
2013-11-01 14:24:32 -0600

    prueba

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="EN" lang="EN" dir="ltr">
+	<head profile="http://gmpg.org/xfn/11">
+		<title>Active Meeting :: Crea juntas f&aacute;cil y r&aacute;pido</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<meta http-equiv="imagetoolbar" content="no" />
+		<link rel="stylesheet" href="template/styles/layout.css" type="text/css" />
+		<script type="text/javascript" src="template/scripts/jquery-1.4.1.min.js"></script>
+	</head>
+	<body id="top">
+	<!-- ####################################################################################################### -->
+		<div class="wrapper col1">
+		  <div id="header">
+			<div id="logo">
+			  <h1><a href="#">Active Meeting</a></h1>
+			  <p>Crea juntas f&aacute;cil y r&aacute;pido</p>
+			</div>
+			<div id="topnav">
+			  <ul>
+				<li><a href="index.php">Inicio</a></li>
+				<li><a href="style-demo.html">Crear Junta</a></li>
+				<li><a href="#">Link 1</a></li>
+				<li><a href="#">Link 2</a></li>
+				<li><a href="#">DropDown</a>
+				  <ul>
+					<li><a href="#">Link 1</a></li>
+					<li><a href="#">Link 2</a></li>
+					<li><a href="#">Link 3</a></li>
+				  </ul>
+				</li>
+			  </ul>
+			</div>
+			<br class="clear" />
+		  </div>
+		</div>
+		<!-- ####################################################################################################### -->
+		<br />
+		<div class="wrapper col2">
+		  <div id="featured_slide">
+			<div id="featured_content">
+				<div class="floater">
+					<h2>&iexcl;Hacer juntas nunca fue tan f&aacute;cil!</h2>
+					<p>Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.</p>
+					<!--<p class="readmore"><a href="#">M&aacute;s informaci&oacute;n &raquo;</a></p>-->
+					<a href="formaBasica2.php"><img src="template/images/demo/junta.png" alt="" /></a>
+				</div>
+			</div>
+		  </div>
+		<!-- ####################################################################################################### -->
+		<div class="wrapper col3">
+		  <div id="container">
+			<div class="homepage">
+			  <ul>
+				<li>
+				  <h2><img src="template/images/demo/60x60.gif" alt="" />Ventaja #1</h2>
+				  <p>Nullamlacus dui ipsum conseque loborttis non euisque morbi penas dapibulum orna. Urnaultrices quis curabitur phasellentesque congue magnis vestibulum quismodo nulla et feugiat. Adipisciniapellentum leo ut consequam ris felit elit id nibh sociis malesuada.</p>
+				  <p class="readmore"><a href="#">Continue Reading &raquo;</a></p>
+				</li>
+				<li>
+				  <h2><img src="template/images/demo/60x60.gif" alt="" />Ventaja #2</h2>
+				  <p>Nullamlacus dui ipsum conseque loborttis non euisque morbi penas dapibulum orna. Urnaultrices quis curabitur phasellentesque congue magnis vestibulum quismodo nulla et feugiat. Adipisciniapellentum leo ut consequam ris felit elit id nibh sociis malesuada.</p>
+				  <p class="readmore"><a href="#">Continue Reading &raquo;</a></p>
+				</li>
+				<li class="last">
+				  <h2><img src="template/images/demo/60x60.gif" alt="" />Ventaja #3</h2>
+				  <p>Nullamlacus dui ipsum conseque loborttis non euisque morbi penas dapibulum orna. Urnaultrices quis curabitur phasellentesque congue magnis vestibulum quismodo nulla et feugiat. Adipisciniapellentum leo ut consequam ris felit elit id nibh sociis malesuada.</p>
+				  <p class="readmore"><a href="#">Continue Reading &raquo;</a></p>
+				</li>
+			  </ul>
+			  <br class="clear" />
+			</div>
+		  </div>
+		</div>
+		<!-- ####################################################################################################### -->
+		<div class="wrapper col4">
+		  <div id="copyright">
+			<p class="fl_left">Copyright &copy; 2013 - Todos los derechos reservados - <a href="#">Active Meeting</a></p>
+			<p class="fl_right">Plantilla original de <a href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+			<br class="clear" />
+		  </div>
+		</div>
+	</body>
+</html>
