e9d74195e6b84108f8f4f2fef8ef2f02bb6bc90c
OblivionWielder <kharon@lethedwellers.com>
2013-11-11 21:48:57 -0600

    prepared testing mode for saveInSession
    Several mods and code cleanup

--- saveInSession.php
+++ saveInSession.php
@@ -1,46 +1,88 @@
 <?php
-
-/**
-* @author mofugeek
-* @copyright 2012
-*/
-/* $objeto = "";
-for($i = 0; $i < count($_POST['myData']); ++$i) {
-	if($_POST['myData'][i]["name"]="nombreJunta")
-		$objeto = $_POST['myData'][i]["value"];
-}*/
-
-//if (isset($_POST['myData'][0]["name"])) {
-//    //echo "This var is set so I will print.";
-//    if($_POST['myData'][0]["name"] == "nombreJunta")
-//    {
-//    	session_destroy();
-//    	session_start();
- //   }
-//}
-//else
-//{
 session_start();
-echo "<pre>";
-print_r($_SESSION);
-echo "</pre>";
-//}
-//echo $_SESSION;
+$testing = 1;
+
+
 for($i=0;$i<count($_POST['myData']);$i++){ 
-//	$output = $output.$_POST['myData'][$i]["name"]; //con esto obtienes el nombre del campo
-//	$output = $output.$_POST['myData'][$i]["value"]; //con esto obtienes el nombre del campo
-//session_start();
-// store session data
 $_SESSION[$_POST['myData'][$i]["name"]]=$_POST['myData'][$i]["value"];
 }
 $output = '';
+if($testing == 0)
+{
+echo "<pre>";
+print_r($_SESSION);
+echo "</pre>";
+}
+//This module was for outputing into a file to see results
+//foreach ($_SESSION as $key=>$val)
+//$output = $output . $key. ": ".$val. "<br>";
+//file_put_contents('file2.txt', $output);
 
-foreach ($_SESSION as $key=>$val)
-$output = $output . $key. ": ".$val. "<br>";
-//$output = print_r($_POST['myData'], true); 
-//$output = print_r($objeto."", true);
-file_put_contents('file2.txt', $output);
 
+
+
+
+
+///TESTING Methods///
+function get_random_string($valid_chars, $length)
+{
+    // start with an empty random string
+    $random_string = "";
+
+    // count the number of chars in the valid chars string so we know how many choices we have
+    $num_valid_chars = strlen($valid_chars);
+
+    // repeat the steps until we've created a string of the right length
+    for ($i = 0; $i < $length; $i++)
+    {
+        // pick a random number from 1 up to the number of valid chars
+        $random_pick = mt_rand(1, $num_valid_chars);
+
+        // take the random character out of the string of valid chars
+        // subtract 1 from $random_pick because strings are indexed starting at 0, and we started picking at 1
+        $random_char = $valid_chars[$random_pick-1];
+
+        // add the randomly-chosen char onto the end of our string so far
+        $random_string .= $random_char;
+    }
+
+    // return our finished random string
+    return $random_string;
+}
+
+function randomDate($start_date, $end_date)
+{
+    // Convert to timetamps
+    $min = strtotime($start_date);
+    $max = strtotime($end_date);
+
+    // Generate random number using above bounds
+    $val = rand($min, $max);
+
+    // Convert back to desired date format
+    return date('Y-m-d H:i:s', $val);
+}
+
+
+
+if($testing)
+{
+echo "SESSION IS BEING OVERWRITTEN RIGHT NOW WITHIN SAVEINSESSION";
+echo "SESSION IS BEING OVERWRITTEN RIGHT NOW WITHIN SAVEINSESSION";
+echo "SESSION IS BEING OVERWRITTEN RIGHT NOW WITHIN SAVEINSESSION";
+
+$_SESSION["accion"] = 1;
+$_SESSION["nombreJunta"] = get_random_string("abcdefghijklmnopqrstuvwxyz", 5); //nombre de la junta
+$_SESSION["emailCreador"] = get_random_string("abcdefghijklmnopqrstuvwxyz", 5)."@yopmail.com"; //nombre de la junta
+$_SESSION["fechaDeCierre"] = randomDate("1970-01-01 01:01","2013-12-30 23:30");
+$_SESSION["descripcionJunta"] = get_random_string("abcdefghijklmnopqrstuvwxyz", 50)."@yopmail.com"; //nombre de la junta
+
+
+echo "SESSION IS BEING OVERWRITTEN RIGHT NOW WITHIN SAVEINSESSION";
+echo "SESSION IS BEING OVERWRITTEN RIGHT NOW WITHIN SAVEINSESSION";
+echo "SESSION IS BEING OVERWRITTEN RIGHT NOW WITHIN SAVEINSESSION";
+
+}
 switch ($_SESSION["accion"]) {
     case 0:
         echo "none";
@@ -76,10 +118,12 @@ function crearJunta($id = 'default'){
 echo $_SESSION['nombreJunta'];
 echo 'segunda Seccion';
 	$nombreDeJunta = filter_var($_SESSION['nombreJunta'], FILTER_SANITIZE_SPECIAL_CHARS); 
-	$emailDelCreador = $_SESSION['emailCreador']; 
-	$fechaDeCierre = $_SESSION['cierreVotacion']; 
-	$horaDeCierre = $_SESSION['horaCierreVotacion']; 
-	$descipcionDeJunta = $_SESSION['descripcionJunta']; 
+	$emailDelCreador = filter_var($_SESSION['emailCreador'], FILTER_SANITIZE_EMAIL); //FILTER_SANITIZE_EMAIL
+	$fechaDeCierre = $_SESSION['cierreVotacion']; //2012-12-23
+	$horaDeCierre = $_SESSION['horaCierreVotacion']; //19:56
+	//$dateSrc = '2007-04-19 12:50 GMT'; 
+    $dateTime = new DateTime($fechaDeCierre." ".$horaDeCierre); 
+	$descipcionDeJunta = filter_var($_SESSION['descripcionJunta'], FILTER_SANITIZE_SPECIAL_CHARS); //blablabla
 	echo $nombreDeJunta;
 	//variable de la segunda pantalla
 	/*
