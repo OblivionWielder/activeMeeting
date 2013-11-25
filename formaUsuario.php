03025d3acf5946917a6c6f7d402bbe4555d9b5c9
Alberto Gomez <albertogomez1991@gmail.com>
2013-11-25 16:12:53 -0600

    archivo formaUsuario.php agregado

---
+++ formaUsuario.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="EN" lang="EN" dir="ltr">
+	<head profile="http://gmpg.org/xfn/11">
+		<title>Active Meeting :: Generar junta</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<meta http-equiv="imagetoolbar" content="no" />
+		<link rel="stylesheet" href="template/styles/layout.css" type="text/css" />
+		<script type="text/javascript" src="template/scripts/jquery-1.4.1.min.js"></script>
+		<script type="text/javascript"></script>
+	</head>
+	
+	<body>
+	</body>
+</html>
