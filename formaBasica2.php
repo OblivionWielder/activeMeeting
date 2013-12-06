bec71ec46311463c3636779b7639eb899ff6b93a
Alberto Gomez <albertogomez1991@gmail.com>
2013-12-05 18:02:18 -0600

    manejando mas pruebas con date2

--- formaBasica2.php
+++ formaBasica2.php
@@ -7,9 +7,9 @@
 		<link rel="stylesheet" href="template/styles/layout.css" type="text/css" />
 		<script type="text/javascript" src="template/scripts/jquery-1.4.1.min.js"></script>
 		<!-- Codigo para el manejo del Datepicker -->
-		<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+		<!--<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 		<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
-		<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+		<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>-->
 		<!--<script>
 			$(function() {
 				$( ".fecha" ).datepicker( { dateFormat: 'yy-mm-dd' } );
