2cfc3eb3f1d2a67a3c42837458adcb9ded0dce90
Alberto Gomez <albertogomez1991@gmail.com>
2013-12-05 19:48:07 -0600

    pruebas 15

--- formaBasica2.php
+++ formaBasica2.php
@@ -230,7 +230,6 @@
 								})
 								
 								$('#agregafecha').on('click', function() {
-									$('.fecha').datePicker();
 									$('<p><input type="text" id="fechaElegir' + i +'" size="20" name="fechaElegir' + i +'" class="fecha" required/>'
 										+'<select name="horaInicio'+ i +'" id="horaInicio'+ i +'" size="1">'
 										+	'<option value="0:00">0:00</option>'
@@ -285,6 +284,7 @@
 										+	'<option value="23:00">23:00</option>'
 										+'</select>'
 										+'<button type="button" href="#" id="borrafecha" class="remove">Borrar Fecha</button></p>').appendTo(scntDiv);
+									$('.fecha').datePicker();
 									fechas = [document.getElementById("fechaElegir"+i), document.getElementById("horaInicio"+i), document.getElementById("horaFin"+i)];
 									calendario.push(fechas);
 									i++;
