21dcd7d1691be822939e53a75877a31881daa48a
Alberto Gomez <albertogomez1991@gmail.com>
2013-12-05 23:04:55 -0600

    funciona picker

--- formaBasica2.php
+++ formaBasica2.php
@@ -230,6 +230,7 @@
 								})
 								
 								$('#agregafecha').on('click', function() {
+									i++;
 									$('<p><input type="text" id="fechaElegir' + i +'" size="20" name="fechaElegir' + i +'" class="fecha" required/>'
 										+'<select name="horaInicio'+ i +'" id="horaInicio'+ i +'" size="1">'
 										+	'<option value="0:00">0:00</option>'
@@ -284,10 +285,9 @@
 										+	'<option value="23:00">23:00</option>'
 										+'</select>'
 										+'<button type="button" href="#" id="borrafecha" class="remove">Borrar Fecha</button></p>').appendTo(scntDiv);
+									$(function() { $( ".fecha" ).datepicker( { dateFormat: 'yy-mm-dd'} );});
 									fechas = [document.getElementById("fechaElegir"+i), document.getElementById("horaInicio"+i), document.getElementById("horaFin"+i)];
 									calendario.push(fechas);
-									i++;
-									$(function() { $( ".fecha" ).datepicker( { dateFormat: 'yy-mm-dd'} );});
 									return false;
 								});
 							});
